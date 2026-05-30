39fe7ac935dc9a142e4a25200f490bab1a061304
Ivan Kartyshov <afk256rec@gmail.com>
2026-05-31 01:34:38 +0500

    Fix startup without App Group container

--- submodules/TelegramUI/Sources/AppDelegate.swift
+++ submodules/TelegramUI/Sources/AppDelegate.swift
@@ -640,9 +640,12 @@ private func extractAccountManagerState(records: AccountRecordsView<TelegramAcco
             isICloudEnabled: buildConfig.isICloudEnabled
         )
         
-        guard let appGroupUrl = maybeAppGroupUrl else {
-            self.mainWindow?.presentNative(UIAlertController(title: nil, message: "Error 2", preferredStyle: .alert))
-            return true
+        let appGroupUrl: URL
+        if let groupUrl = maybeAppGroupUrl {
+            appGroupUrl = groupUrl
+        } else {
+            let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+            appGroupUrl = URL(fileURLWithPath: documentsPath)
         }
         
         var isDebugConfiguration = false
